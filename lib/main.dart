a8b9b92d932e89e6c08173fd5aaf068671d72542
Andres2kk <contato.andres2k@gmail.com>
2023-11-04 16:49:09 -0300

    Counter-Click

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:teste22/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:teste22/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:teste22/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:teste22/presentation/screens/counter/counter_screen.dart';
+// import 'package:teste22/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+        home: const CounterFunctionsScreen());
+  }
+}
